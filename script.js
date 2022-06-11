18a7b9700d5fb4713a2e866a6ce1dfb4b1a95199
Sun Space <cmullan@Connors-MacBook-Air.local>
2022-06-10 17:21:44 -0700

    Created variables, if statements withing generate password and started for loop.

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+
+
+var combinedArray = [];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters =["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
+function generatePassword(){
+
+  var passwordLength = prompt("How many Characters would you like?");
+
+  if (passwordLength < 8 || passwordLength >  128) {
+  alert("Password must be between 8 & 128 characters");
+  return " ";
+  }
+
+ 
+  var numberChoice = confirm("Click OK to confirm you want to use numbers.");
+
+  var characterChoice =confirm("Click OK to confirm you want to use special characters.");
+
+  var upperChoice =confirm("Click OK to confirm you want to use upper case.");
+
+  var lowerChoice =confirm("Click OK to confirm you want to use lower case");
+
+  
+  if(!numberChoice && !characterChoice && !upperChoice && !lowerChoice) {
+  alert("Must use at least one.")
+  return " ";
+  }
+
+  else if(numberChoice && characterChoice && upperCase && lowerCase) {
+
+    combinedArray = combinedArray.concat(numbers, specialCharacters, upperCase, lowerCase);
+
+    var result1 = '';
+
+    var combinedArrayLength = combinedArray.length;
+
+    for ( i=0; i<passwordLength; i++) {
+   
+      result1 += combinedArray.charAt(Math.floor(Math.random() * combinedArrayLength));
+      
+      console.log(result1)
+   
+     }
+  
+  }
+  
+
+  else if(!numberChoice && characterChoice && upperCase && lowerCase){
+
+  }
+
+  else if(!numberChoice && characterChoice && upperCase && lowerCase){
+    
+  }
+
+  else if(!numberChoice && characterChoice && upperCase && lowerCase){
+    
+  }
+
+  else if(!numberChoice && characterChoice && upperCase && lowerCase){
+    
+  }
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +83,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
